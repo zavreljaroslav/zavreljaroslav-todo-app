9532c0a2e2fd7db34dff00809ec31a3d52a4ea2d
Jaroslav Zavrel <zavreljaroslav@email.cz>
2021-12-11 16:57:06 +0100

    moved handleInput to separate class

--- src/ToDo/Main.java
+++ src/ToDo/Main.java
@@ -4,35 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         ToDo todo = new ToDo();
-        handleInput(args, todo);
-        todo.listAllTasks();
-    }
-
-    public static void handleInput(String[] args, ToDo todo) { //checks if the input is valid, if yes, executes the function
-        if (args.length == 0) {
-            todo.printUsage();
-        } else if (args[0].equals("-a")) {
-            if (args.length == 1) {
-                System.out.println("Couldn't add task: no index provided!");
-            } else {
-                todo.addTask(args[1]);
-            }
-        } else if (args[0].equals("-l")) {
-            todo.listAllTasks();
-        } else if (args[0].equals(("-r"))){
-            if (args.length == 1) {
-                System.out.println("Couldn't remove a task: no task selected for removal.");
-            } else {
-                todo.removeTask(args[1]);
-            }
-        } else if (args[0].equals("-c")){
-            if (args.length == 1){
-                System.out.println("Unable to check, no index provided");
-            } else {
-                todo.checkTask(args[1]);
-            }
-        } else {
-            System.out.println("Unsupported argument");
-        }
+        HandleInput.handleInput(args,todo);
     }
 }
+
