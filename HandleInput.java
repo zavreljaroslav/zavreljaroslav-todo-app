bedb598e69ca66f2b89ad9b21745f06582b437ad
zavreljaroslav <89843382+zavreljaroslav@users.noreply.github.com>
2021-12-11 16:59:23 +0100

    Add files via upload

---
+++ HandleInput.java
@@ -0,0 +1,31 @@
+package ToDo;
+
+public class HandleInput {
+    public static void handleInput(String[] args, ToDo todo) { //checks if the input is valid, if yes, executes the function
+        if (args.length == 0) {
+            todo.printUsage();
+        } else if (args[0].equals("-a")) {
+            if (args.length == 1) {
+                System.out.println("Couldn't add task: no index provided!");
+            } else {
+                todo.addTask(args[1]);
+            }
+        } else if (args[0].equals("-l")) {
+            todo.listAllTasks();
+        } else if (args[0].equals(("-r"))){
+            if (args.length == 1) {
+                System.out.println("Couldn't remove a task: no task selected for removal.");
+            } else {
+                todo.removeTask(args[1]);
+            }
+        } else if (args[0].equals("-c")){
+            if (args.length == 1){
+                System.out.println("Unable to check, no index provided");
+            } else {
+                todo.checkTask(args[1]);
+            }
+        } else {
+            System.out.println("Unsupported argument");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package ToDo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ToDo todo = new ToDo();
+        HandleInput.handleInput(args,todo);
+    }
+}
+
